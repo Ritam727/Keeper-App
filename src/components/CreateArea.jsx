a1fc48b72d19ab578b592ad6bcc8bb35fb1f071f
Ritam727 <b20127@students.iitmandi.ac.in>
2021-08-02 09:21:50 +0530

    Finalised

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
+import Zoom from '@material-ui/core/Zoom';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
 
 function CreateArea(props) {
+  const [focus, setFocus] = useState(false);
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   return (
     <div>
       <form
+        className="create-note"
         onSubmit={(event) => {
           props.onAdd({ title: title, content: content });
           setTitle("");
@@ -18,15 +23,19 @@ function CreateArea(props) {
           placeholder="Title"
           onChange={(event) => setTitle(event.target.value)}
           value={title}
+          style={{display: focus ? "inline" : "none"}}
         />
         <textarea
           name="content"
           placeholder="Take a note..."
-          rows="3"
+          rows={focus ? 3 : 1}
           onChange={(event) => setContent(event.target.value)}
           value={content}
+          onClick={() => setFocus(true)}
         />
-        <button type="submit">Add</button>
+        <Zoom in={focus ? true : false}>
+          <Fab type="submit"><AddIcon /></Fab>
+        </Zoom>
       </form>
     </div>
   );
